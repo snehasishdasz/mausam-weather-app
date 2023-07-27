d3afe00e9691fb2e61d0c1aa51d4df4cdd045ae5
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-07-27 19:08:57 +0530

    major-Change

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
-import { useEffect } from "react";
-import hotBg from "./assests/1s.jpg"
-import coldBg from "./assests/1w.jpg"
-import Description from "./components/Description";
-import { getFormatedWeatherData } from "./WeatherService";
+import React from 'react'
 
-
-function App() {
-
-  useEffect(()=>{
-
-    const fetchWeatherData = async ()=>{
-      const data = await getFormatedWeatherData('paris');
-      console.log(data);
-    };
-    fetchWeatherData();
-  },[]);
-
-
-
-
+const App = () => {
   return (
-    <div className="App" style={{backgroundImage: `url(${coldBg})`}}>
-    <div className="overlay">
-      <div className="container">
-        <div className="section section__inputs">
-          <input type="text" name="city" placeholder="Enter City..."/>
-          <button>°F</button>
-        </div>
-
-        <div className="section section__temprature ">
-          <div className="icon">
-            <h3>London GB</h3>
-            <img src="https://cdn-icons-png.flaticon.com/128/2698/2698250.png" alt="weather icon" />
-            <h3>Cloudy</h3>
-          </div>
-          <div className="temprature">
-            <h1>34 °C</h1>
-          </div>
-        </div>
-        <Description/>
-      </div>
-    </div>
-      
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
